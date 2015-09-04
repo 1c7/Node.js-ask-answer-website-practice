5232c89a44866fe83eb8f5aa5ad08400b455b03e
¨Cheng <guokrfans@gmail.com>
2015-09-04 20:38:23 +0800

    version 1

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <br>
 ### 具体用啥？
 
-Node.js + [Express.js](http://expressjs.com/) + [Jade](http://jade-lang.com/reference/case/)  
+Node.js + [Express.js 4.13.3](http://expressjs.com/) + [Jade](http://jade-lang.com/reference/case/)  
 MongoDB  
 Linux(Ubuntu 14.04)  
 
@@ -28,22 +28,32 @@ views  模板文件
 
 
 <br>
-### 数据库设计
-占个位置下次写
+### MongoDB 数据库设计(这个会根据情况不断改)
+
+database name : ask-answer  
+
+<br>
+
+collection:
+    q&a - 存所有的问题和回答数据
+    user - 存用户数据
 
 
 <br>
-### 学到的东西 & 踩过的坑(把觉得对大家有用的东西都列一下)
+### 学到的东西 & 踩过的坑(把对大家可能有用的都列一下)
 
+<br>
 ##### 1. 如果 Express.js 用 Jade 提示失败。  
 那就是你的 Express.js 和 Jade 一个安装在本地，一个全局安装 (-g)  
 造成了这种结果。  
 
+<br>
 ##### 2. Jade 的变量语法是 #{name}  
 如果要文本输出, 记得前面加个 |  
 不然 #{name} 就变成个标签了  
 比如 name = dave, 就会变成 <dave></dave>, 页面上你是看不到标签的, 看下网页源代码你就知道了  
 
+<br>
 ##### 2. node_module 目录不用上传  
 浪费 git push 的时间阿  
 从 git 里移除掉的方法(不是删到回收站去, 只是让 git 不要对这个目录作版本管理了):  
@@ -52,6 +62,7 @@ views  模板文件
 
 ref: http://stackoverflow.com/questions/1143796/remove-a-file-from-a-git-repository-without-deleting-it-from-the-local-filesyste
 
+<br>
 ##### 3. Ubuntu 操作 git 提交的时候连末尾带 ~ 符号的文件也提交上去了
 比如有个 README.MD  还有个 README.MD~
 查了资料发现那是 gedit 的文件备份, 
@@ -94,8 +105,48 @@ http://stackoverflow.com/questions/15606955/how-can-i-make-git-show-a-list-of-th
     git rm views/submit.jade~
 
 
+别忘了 git add, git commit, git push
 
-##### 4. package.json 是
+<br>
+##### 4. package.json 的标准在?
+
+http://blog.nodejitsu.com/package-dependencies-done-right/  
+http://browsenpm.org/help  
+
+
+<br>
+##### 5. 查看版本
+想看本地安装的所有包的版本, 用:
+    
+    npm list --depth 0
+
+来源: http://stackoverflow.com/questions/10972176/find-the-version-of-an-installed-npm-package  
+
+<br>
+##### 6. 拿 POST 参数
+文档里有说具体方法  
+http://expressjs.com/4x/api.html#req.body  
+我概括下:  
+
+    npm install body-parser  
+    var bodyParser = require('body-parser');  in your xxx.js file  
+
+然后接收的地方这样写:
+
+    app.post('/handle_submit', function (req, res) {
+        var t = req.body.title;
+        // 拿 POST 里的 title 参数
+        // 如果没传这个参数, t 会等于 ''
+        res.send(t);
+    });
+
+    
+### 框架比较
+https://www.airpair.com/node.js/posts/nodejs-framework-comparison-express-koa-hapi
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var mongoose = require('mongodb');
 //var jade = require('jade');
+var bodyParser = require('body-parser');
+
 
 /*
-  路由
+    配置
+*/
+app.use(bodyParser.json()); 
+// for parsing application/json
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+// for parsing application/x-www-form-urlencoded
+
+// 设定views变量，意为视图存放的目录
+app.set('views', path.join(__dirname, 'views'));
+
+// 设定view engine变量，意为网页模板引擎
+app.set('view engine', 'jade');
+
+// 设定静态文件目录，比如本地文件
+// 目录为demo/public/images，访问
+// 网址则显示为http://localhost:3000/images
+app.use(express.static('public'));
+
+
+
+
+/*
+    数据库
+*/
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+var db = require('mongodb').Db;
+// https://mongodb.github.io/node-mongodb-native/api-generated/collection.html
+
+
+var port = '27017';
+var db_url = "mongodb://localhost:"+port+"/ask-answer";
+
+// 连一下测试一下
+MongoClient.connect(db_url, function(err, db) {
+  if(!err) {
+    console.log('Successfully connect to', db_url);
+  }
+});
+
+
+
+/*
+    路由
 */
 
-
+/*
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+*/
 
 // 提交页面
 app.get('/submit', function (req, res) {
@@ -23,43 +69,249 @@ app.get('/submit', function (req, res) {
 });
 
 
-// 查看页面
-app.get('/list', function (req, res) {
-  res.sendfile('./views/display.html');
+// https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
+
+
+
+// 处理提交页面
+app.post('/h_submit', function (req, res) {
+
+    // 接收和检查问题标题
+     var t = req.body.title;
+     if(t == ''){
+        res.send('必须输入问题的标题!');
+        return;
+     }
+     
+     
+     // 问题描述
+     var d = req.body.desc;
+     if( d == ''){
+        var doc = {'title':t}
+     }else{
+        var doc = {'title':t, 'desc':d}
+     }
+
+
+    // 生成一个问题ID, 问题详情页需要用到
+    /*
+        打算:
+        格式是 000000  6位数
+        比如  230123
+        我们不从 000001 开始, 
+        因为这样容易被爬虫折腾, 随便哪个人都能写个程序遍历所有页面
+        而且也容易让人知道网站到底有多少个问题
+        比如 002301  就是有 2301 个问题
+        
+        1. 6位
+        2. 全数字
+        3. 不重复
+        4. 不连续
+        
+        实际:
+        我好懒啊，先用时间戳顶着吧，以后再改;
+    */
+
+    var qid = Date.now();
+    doc['qid'] = qid;
+    
+    console.log(doc);
+
+
+    // 存 MongoDB
+    MongoClient.connect(db_url, function(err, db) {
+        if(err) { return console.dir(err); }
+
+        var collection = db.collection('qa');
+        collection.insert(doc);
+        
+    });
+
+    
+    // insert 怎么检测成功失败?
+    // https://mongodb.github.io/node-mongodb-native/api-articles/nodekoarticle1.html
+    res.redirect('/'); // 302 重定向
+    //res.send('ok');
+
+
 });
 
 
-// 设定views变量，意为视图存放的目录
-app.set('views', path.join(__dirname, 'views'));
+
+// 问题列表页
+app.get('/', function (req, res) {
+
+    // Connect to the db
+    MongoClient.connect(db_url, function(err, db) {
+        if(err) { return console.dir(err); }
+
+        //var r = db.qa.find();
+        var collection = db.collection('qa');
+
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (result.length) {
+            console.log('Found:', result);
+            res.render('display', { 'r': result });
+          } else {
+            res.render('display', { 'r': result });
+          }
+          //Close connection
+          db.close();
+        });
+    
+        //console.log(r);
+        //
+    });
+
+    
+});
+
+
+
+// 问题详情页
+app.get('/question/:id', function (req, res) {
+
+    var qid = req.params.id;
+    // quesiton id
+    // typeof qid === string
+    
+    // 检测参数是否合法
+    // 1. question id 必须是全数字的. 不得包含任何字母或特殊符号
+    // 2. id 长度必须符合特定位数, 不然也不用查数据库了.
+    // 我们现在用的是时间戳, 比如 1441365036033, 一共13位
+    // 如果不合法则 404
+    // 如果合法就展示问题详情
+    
+    if(isNumeric(qid) === false){
+        res.send('参数错误, question id 必须是全数字');
+        return;
+    }
+    
+    if(qid.length != 13){
+        res.send('quiestion id 位数不对, 应该是13位');
+        return;
+    }
+
+    qid = Number(qid);
+    // MongoDB 里 qid 是数字, 这里是字符串
+    // 不转类型的话数据库会找不到的
+
+    MongoClient.connect(db_url, function(err, db) {
+        if(err) { return console.dir(err); }
+
+        var collection = db.collection('qa');
+
+        collection.find({'qid':qid}).toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (result.length) {
+            console.log('Found:', result);
+            res.render('question', { 'r': result[0] });
+          } else {
+           // get nothing
+            //res.render('display', { 'r': result });
+          }
+          //Close connection
+          db.close();
+        });
+    
+        //console.log(r);
+        //
+    });
+
+    
+});
+
+
+// 处理评论
+app.post('/comment', function (req, res) {
+
+     // 接收和检查问题标题
+     var c = req.body.comment;
+     var qid = req.body.qid;
+     if(c === '' || qid === ''){
+        res.send('参数不足, 无法评论');
+        return;
+     }
+     
+    qid = Number(qid);
+    // MongoDB 里 qid 是数字, 这里是字符串
+    // 不转类型的话数据库会找不到的
+     
+    // 存 MongoDB
+    MongoClient.connect(db_url, function(err, db) {
+        if(err) { return console.dir(err); }
+
+        var collection = db.collection('qa');
+        collection.update( {'qid':qid}, {$push: {comments: c}} );
+        
+    });
+     
+    res.redirect(req.get('referer'));
+    // 刷新页面
+});
 
 
 
 
-// 设定view engine变量，意为网页模板引擎
-app.set('view engine', 'jade');
 
 
+// 测试页
+// 比如你想测一个随机数生成算法, 想在网页看到效果，就用这个
+app.get('/test', function (req, res) {
 
-// 设定静态文件目录，比如本地文件
-// 目录为demo/public/images，访问
-// 网址则显示为http://localhost:3000/images
-app.use(express.static('public'));
+/*
+    思路
+    看mongodb里有没有那个数字
+*/
+    // Date.now() == 1441364199786
+    // 类似这样的
+    res.send('a'+Date.now());
+    return;
 
 
+    MongoClient.connect(db_url, function(err, db) {
+        if(err) { return console.dir(err); }
+
+        var collection = db.collection('qid');
+
+        collection.find().toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (result.length) {
+            console.log('Found:', result);
+            res.render('display', { 'r': result });
+          } else {
+            res.render('display', { 'r': result });
+          }
+          //Close connection
+          db.close();
+        });
+    
+        //console.log(r);
+        //
+    });
+    
+});
 
 
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('listening at http://%s:%s', host, port);
 });
 
 
 
 
-// 怎么和数据库交互?
-// 问题: jade 模板管用吗?
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+// http://stackoverflow.com/questions/18082/validate-decimal-numbers-in-javascript-isnumeric
+
 
 
 
